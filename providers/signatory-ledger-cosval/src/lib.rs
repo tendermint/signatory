d4e3f45499c30ee961512eda4b9913455750c33c
Juan Leni <lenijuan@gmail.com>
2018-09-08 16:22:20 +0200

    rewiring traits

--- providers/signatory-ledger-cosval/src/lib.rs
+++ providers/signatory-ledger-cosval/src/lib.rs
@@ -12,19 +12,22 @@ html_root_url = "https://docs.rs/signatory-ledger-cosval/0.0.1"
 extern crate signatory;
 extern crate ledger_cosmos_rs;
 
+use std::sync::Mutex;
+use std::sync::Arc;
+
 use signatory::{
-//    ed25519::Ed25519Signature,
-ed25519::PublicKey,
-//    generic_array::{typenum::U32, GenericArray},
-error::{Error, ErrorKind},
-//    Signature,
-PublicKeyed,
-//    Signer,
+    ed25519::Ed25519Signature,
+    ed25519::PublicKey,
+    generic_array::{typenum::U32, GenericArray},
+    error::{Error, ErrorKind},
+    PublicKeyed,
+    Signer,
 };
 
 /// ed25519 signature provider for the ledger cosmos validator app
+#[allow(dead_code)]
 pub struct Ed25519CosmosSigner {
-//    app: ledger_cosmos_rs::CosmosValidatorApp
+    app: Arc<Mutex<ledger_cosmos_rs::CosmosValidatorApp>>
 }
 
 impl Ed25519CosmosSigner {
@@ -33,7 +36,10 @@ impl Ed25519CosmosSigner {
         // TODO: Maybe use this to pass other derivation path
 
         match ledger_cosmos_rs::CosmosValidatorApp::connect() {
-            Ok(_x) => Ok(Ed25519CosmosSigner { /*app*/ }),
+            Ok(_x) => {
+                let app = Arc::new(Mutex::new(_x));
+                Ok(Ed25519CosmosSigner { app })
+            },
             Err(_x) => Err(
                 Error::new(ErrorKind::ProviderError, Some("Unknown"))
             )
@@ -42,20 +48,32 @@ impl Ed25519CosmosSigner {
 }
 
 impl PublicKeyed<PublicKey> for Ed25519CosmosSigner {
-    /// TODO: Return the public key that corresponds to the private key for this signer
+    /// Returns the public key that corresponds cosmos validator app connected to this signer
     fn public_key(&self) -> Result<PublicKey, Error> {
-        let pk = [0u8; 32];
-        Ok(PublicKey(pk))
+        let app = self.app.lock().unwrap();
+
+        match app.public_key() {
+            Ok(pk) => Ok(PublicKey(pk)),
+            Err(_x) => Err(
+                Error::new(ErrorKind::ProviderError, Some("Unknown"))
+            )
+        }
     }
 }
 
-//impl Signer<GenericArray<u8, U32>, Ed25519Signature> for Ed25519CosmosSigner {
-//    /// c: Compute a compact, fixed-sized signature of the given 32-byte SHA-256 digest
-//    fn sign(&self, _msg: GenericArray<u8, U32>) -> Result<Ed25519Signature, Error> {
-//        let signature = [0u8; 64];
-//        Ok(Ed25519Signature(signature))
-//    }
-//}
+impl Signer<GenericArray<u8, U32>, Ed25519Signature> for Ed25519CosmosSigner {
+    /// c: Compute a compact, fixed-sized signature of the given 32-byte SHA-256 digest
+    fn sign(&self, msg: GenericArray<u8, U32>) -> Result<Ed25519Signature, Error> {
+        let app = self.app.lock().unwrap();
+
+        match app.sign(&msg) {
+            Ok(sig) => Ok(Ed25519Signature(sig)),
+            Err(_x) => Err(
+                Error::new(ErrorKind::ProviderError, Some("Unknown"))
+            )
+        }
+    }
+}
 
 // TODO: test against actual test vectors, rather than just checking if signatures roundtrip
 #[cfg(test)]
