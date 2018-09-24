012e00ac4918bae361dd8228fa18489e6528c8b6
Tony Arcieri <tony@iqlusion.io>
2018-09-24 10:56:44 -0700

    nistp384: linewrap b constant

--- src/curve/nistp384/mod.rs
+++ src/curve/nistp384/mod.rs
@@ -18,7 +18,8 @@ pub use self::test_vectors::SHA384_FIXED_SIZE_TEST_VECTORS;
 /// The NIST P-384 elliptic curve: y² = x³ - 3x + b over a ~384-bit prime field
 /// where b is "verifiably random"† constant:
 ///
-/// b = 27580193559959705877849011840389048093056905856361568521428707301988689241309860865136260764883745107765439761230575
+/// b = 2758019355995970587784901184038904809305690585636156852142
+///     8707301988689241309860865136260764883745107765439761230575
 ///
 /// † NOTE: the specific origins of this constant have never been fully disclosed
 ///   (it is the SHA-1 digest of an inexplicable NSA-selected constant)
