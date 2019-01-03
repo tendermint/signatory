ced3000a22a2f88c75abf2c5f8c7b76c66267547
Juan Leni <lenijuan@gmail.com>
2019-01-03 19:05:01 +0100

    upgrading to ledger validator app 0.2.1

--- signatory-ledger-cosval/src/lib.rs
+++ signatory-ledger-cosval/src/lib.rs
@@ -23,12 +23,12 @@ pub struct Ed25519CosmosAppSigner {
 impl Ed25519CosmosAppSigner {
     /// Create a new ed25519 signer based on ledger nano s - cosmos validator app
     pub fn connect() -> Result<Self, Error> {
-        // TODO: Maybe use this to pass other derivation path
-
         match ledger_cosmos::CosmosValidatorApp::connect() {
             Ok(validator_app) => {
                 let app = Arc::new(Mutex::new(validator_app));
-                Ok(Ed25519CosmosAppSigner { app })
+                let signer = Ed25519CosmosAppSigner { app };
+                let _pk = signer.public_key().unwrap();
+                Ok(signer)
             }
             Err(err) => Err(Error::new(ErrorKind::ProviderError, Some(&err.to_string()))),
         }
@@ -61,8 +61,6 @@ impl Signer<Signature> for Ed25519CosmosAppSigner {
 
 #[cfg(test)]
 mod tests {
-    // TODO: Improve tests once amino schema is defined
-
     #[test]
     fn public_key() {
         use crate::Ed25519CosmosAppSigner;
@@ -70,6 +68,7 @@ mod tests {
         let signer = Ed25519CosmosAppSigner::connect().unwrap();
 
         let _pk = signer.public_key().unwrap();
+        println!("PK {:0X?}", _pk);
     }
 
     #[test]
@@ -78,7 +77,114 @@ mod tests {
         use signatory::Signer;
 
         let signer = Ed25519CosmosAppSigner::connect().unwrap();
-        let some_message1 = b"{\"height\":1,\"other\":\"Some dummy data\",\"round\":0}";
-        let _sig = signer.sign(some_message1).unwrap();
+
+        // Sign message1
+        let some_message1 = [
+            0x8,                                    // (field_number << 3) | wire_type
+            0x1,                                    // PrevoteType
+            0x11,                                   // (field_number << 3) | wire_type
+            0x10, 0x00, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, // height
+            0x19,                                   // (field_number << 3) | wire_type
+            0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, // round
+            0x22, // (field_number << 3) | wire_type
+            // remaining fields (timestamp):
+            0xb, 0x8, 0x80, 0x92, 0xb8, 0xc3, 0x98, 0xfe, 0xff, 0xff, 0xff, 0x1];
+
+        match signer.sign(&some_message1) {
+            Ok(_sig) => {}
+            Err(e) => { println!("Err {:#?}", e); }
+        }
+
+        // Sign message2
+        let some_message2 = [
+            0x8,                                    // (field_number << 3) | wire_type
+            0x1,                                    // PrevoteType
+            0x11,                                   // (field_number << 3) | wire_type
+            0x10, 0x00, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, // height
+            0x19,                                   // (field_number << 3) | wire_type
+            0x2, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, // round
+            0x22, // (field_number << 3) | wire_type
+            // remaining fields (timestamp):
+            0xb, 0x8, 0x80, 0x92, 0xb8, 0xc3, 0x98, 0xfe, 0xff, 0xff, 0xff, 0x1];
+
+        match signer.sign(&some_message2) {
+            Ok(_sig) => {}
+            Err(e) => { println!("Err {:#?}", e); }
+        }
     }
+
+    #[test]
+    fn sign2() {
+        use signatory::Signer;
+        use Ed25519CosmosAppSigner;
+
+        let signer = Ed25519CosmosAppSigner::connect().unwrap();
+
+        // Sign message1
+        let some_message1 = [
+            0x8,                                    // (field_number << 3) | wire_type
+            0x1,                                    // PrevoteType
+            0x11,                                   // (field_number << 3) | wire_type
+            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x0F, // height
+            0x19,                                   // (field_number << 3) | wire_type
+            0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, // round
+            0x22, // (field_number << 3) | wire_type
+            // remaining fields (timestamp):
+            0xb, 0x8, 0x80, 0x92, 0xb8, 0xc3, 0x98, 0xfe, 0xff, 0xff, 0xff, 0x1];
+
+        match signer.sign(&some_message1) {
+            Ok(_sig) => {}
+            Err(e) => { println!("Err {:#?}", e); }
+        }
+
+        // Sign message2
+        let some_message2 = [
+            0x8,                                    // (field_number << 3) | wire_type
+            0x1,                                    // PrevoteType
+            0x11,                                   // (field_number << 3) | wire_type
+            0x10, 0x00, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, // height
+            0x19,                                   // (field_number << 3) | wire_type
+            0x2, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, // round
+            0x22, // (field_number << 3) | wire_type
+            // remaining fields (timestamp):
+            0xb, 0x8, 0x80, 0x92, 0xb8, 0xc3, 0x98, 0xfe, 0xff, 0xff, 0xff, 0x1];
+
+        match signer.sign(&some_message2) {
+            Ok(_sig) => {}
+            Err(e) => { println!("Err {:#?}", e); }
+        }
+    }
+
+    #[test]
+    fn sign_many() {
+        use signatory::PublicKeyed;
+        use signatory::Signer;
+        use Ed25519CosmosAppSigner;
+
+        let signer = Ed25519CosmosAppSigner::connect().unwrap();
+
+        // Get public key to initialize
+        let _pk = signer.public_key().unwrap();
+        println!("PK {:0X?}", _pk);
+
+        for index in 50u8..254u8 {
+            // Sign message1
+            let some_message = [
+                0x8,                                    // (field_number << 3) | wire_type
+                0x1,                                    // PrevoteType
+                0x11,                                   // (field_number << 3) | wire_type
+                0x40, 0x00, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, // height
+                0x19,                                   // (field_number << 3) | wire_type
+                index, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, // round
+                0x22, // (field_number << 3) | wire_type
+                // remaining fields (timestamp):
+                0xb, 0x8, 0x80, 0x92, 0xb8, 0xc3, 0x98, 0xfe, 0xff, 0xff, 0xff, 0x1];
+
+            match signer.sign(&some_message) {
+                Ok(_sig) => {}
+                Err(e) => { println!("Err {:#?}", e); }
+            }
+        }
+    }
+
 }
