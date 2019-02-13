e827e57c51a1d45b419eb8e86f7832444e29e0ec
Juan Leni <juan.leni@zondax.ch>
2019-02-13 12:07:01 +0100

    Update lib.rs

--- signatory-ledger-tm/src/lib.rs
+++ signatory-ledger-tm/src/lib.rs
@@ -4,7 +4,7 @@
 #![deny(unsafe_code, unused_import_braces, unused_qualifications)]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tendermint/signatory/master/img/signatory-rustacean.png",
-    html_root_url = "https://docs.rs/signatory-ledger-cosval/0.11.0"
+    html_root_url = "https://docs.rs/signatory-ledger-tm/0.11.0"
 )]
 
 use std::sync::{Arc, Mutex};
@@ -17,13 +17,13 @@ use signatory::{
 
 /// ed25519 signature provider for the Ledger Tendermint Validator app
 pub struct Ed25519LedgerTmAppSigner {
-    app: Arc<Mutex<ledger_cosmos::CosmosValidatorApp>>,
+    app: Arc<Mutex<ledger_tendermint::TendermintValidatorApp>>,
 }
 
 impl Ed25519LedgerTmAppSigner {
     /// Create a new Ed25519 signer based on Ledger Nano S - Tendermint Validator app
     pub fn connect() -> Result<Self, Error> {
-        match ledger_cosmos::CosmosValidatorApp::connect() {
+        match ledger_tendermint::TendermintValidatorApp::connect() {
             Ok(validator_app) => {
                 let app = Arc::new(Mutex::new(validator_app));
                 let signer = Ed25519LedgerTmAppSigner { app };
@@ -81,7 +81,7 @@ mod tests {
 
         // Sign message1
         let some_message1 = [
-            0x8,  // (field_number << 3) | wire_type
+            33, 0x8,  // (field_number << 3) | wire_type
             0x1,  // PrevoteType
             0x11, // (field_number << 3) | wire_type
             0x10, 0x00, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,  // height
@@ -92,32 +92,7 @@ mod tests {
             0xb, 0x8, 0x80, 0x92, 0xb8, 0xc3, 0x98, 0xfe, 0xff, 0xff, 0xff, 0x1,
         ];
 
-        match signer.sign(&some_message1) {
-            Ok(_sig) => {}
-            Err(e) => {
-                println!("Err {:#?}", e);
-            }
-        }
-
-        // Sign message2
-        let some_message2 = [
-            0x8,  // (field_number << 3) | wire_type
-            0x1,  // PrevoteType
-            0x11, // (field_number << 3) | wire_type
-            0x10, 0x00, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,  // height
-            0x19, // (field_number << 3) | wire_type
-            0x2, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,  // round
-            0x22, // (field_number << 3) | wire_type
-            // remaining fields (timestamp):
-            0xb, 0x8, 0x80, 0x92, 0xb8, 0xc3, 0x98, 0xfe, 0xff, 0xff, 0xff, 0x1,
-        ];
-
-        match signer.sign(&some_message2) {
-            Ok(_sig) => {}
-            Err(e) => {
-                println!("Err {:#?}", e);
-            }
-        }
+        signer.sign(&some_message1).unwrap();
     }
 
     #[test]
@@ -128,10 +103,10 @@ mod tests {
 
         // Sign message1
         let some_message1 = [
-            0x8,  // (field_number << 3) | wire_type
+            33, 0x8,  // (field_number << 3) | wire_type
             0x1,  // PrevoteType
             0x11, // (field_number << 3) | wire_type
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x0F, // height
+            0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,  // height
             0x19, // (field_number << 3) | wire_type
             0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,  // round
             0x22, // (field_number << 3) | wire_type
@@ -139,16 +114,11 @@ mod tests {
             0xb, 0x8, 0x80, 0x92, 0xb8, 0xc3, 0x98, 0xfe, 0xff, 0xff, 0xff, 0x1,
         ];
 
-        match signer.sign(&some_message1) {
-            Ok(_sig) => {}
-            Err(e) => {
-                println!("Err {:#?}", e);
-            }
-        }
+        signer.sign(&some_message1).unwrap();
 
         // Sign message2
         let some_message2 = [
-            0x8,  // (field_number << 3) | wire_type
+            33, 0x8,  // (field_number << 3) | wire_type
             0x1,  // PrevoteType
             0x11, // (field_number << 3) | wire_type
             0x10, 0x00, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,  // height
@@ -159,12 +129,7 @@ mod tests {
             0xb, 0x8, 0x80, 0x92, 0xb8, 0xc3, 0x98, 0xfe, 0xff, 0xff, 0xff, 0x1,
         ];
 
-        match signer.sign(&some_message2) {
-            Ok(_sig) => {}
-            Err(e) => {
-                println!("Err {:#?}", e);
-            }
-        }
+        signer.sign(&some_message2).unwrap();
     }
 
     #[test]
@@ -182,7 +147,7 @@ mod tests {
         for index in 50u8..254u8 {
             // Sign message1
             let some_message = [
-                0x8,  // (field_number << 3) | wire_type
+                33, 0x8,  // (field_number << 3) | wire_type
                 0x1,  // PrevoteType
                 0x11, // (field_number << 3) | wire_type
                 0x40, 0x00, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,  // height
@@ -197,6 +162,7 @@ mod tests {
                 Ok(_sig) => {}
                 Err(e) => {
                     println!("Err {:#?}", e);
+                    panic!()
                 }
             }
         }
