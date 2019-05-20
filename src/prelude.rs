2867d5f4369a04302eca7f3129e77d0e3cc55d9c
Tony Arcieri <tony@iqlusion.io>
2019-05-20 11:30:00 -0700

    Support stable 'alloc' API
    Updates the 'alloc'-related boilerplate to support usage with the
    recently stabilized feature.

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,7 +1,10 @@
 //! Use either of `std` prelude or `alloc` prelude (latter only on nightly)
 
 #[cfg(all(feature = "alloc", not(feature = "std")))]
-pub use alloc::prelude::*;
+pub use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
 
 #[cfg(feature = "std")]
 pub use std::prelude::v1::*;
