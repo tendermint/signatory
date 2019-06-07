e42964ceb69d7bb4db10dc3d2209260b1283a854
Tony Arcieri <tony@iqlusion.io>
2019-06-07 06:52:31 -0700

    v0.12.0

--- signatory-ledger-tm/src/lib.rs
+++ signatory-ledger-tm/src/lib.rs
@@ -4,7 +4,7 @@
 #![deny(unsafe_code, unused_import_braces, unused_qualifications)]
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/tendermint/signatory/develop/img/signatory-rustacean.png",
-    html_root_url = "https://docs.rs/signatory-ledger-tm/0.11.0"
+    html_root_url = "https://docs.rs/signatory-ledger-tm/0.12.0"
 )]
 
 use ledger_tendermint::ledgertm::TendermintValidatorApp;
