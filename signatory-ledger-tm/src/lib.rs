8c5fb53d781a06ad7ba00eb44e52db801a093545
Tony Arcieri <tony@iqlusion.io>
2020-05-12 09:11:41 -0700

    signatory-ledger-tm v0.19.1

--- signatory-ledger-tm/src/lib.rs
+++ signatory-ledger-tm/src/lib.rs
@@ -2,7 +2,7 @@
 
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/iqlusioninc/signatory/develop/img/signatory-rustacean.png",
-    html_root_url = "https://docs.rs/signatory-ledger-tm/0.19.0"
+    html_root_url = "https://docs.rs/signatory-ledger-tm/0.19.1"
 )]
 #![deny(unsafe_code)]
 #![warn(missing_docs, rust_2018_idioms, unused_qualifications)]
